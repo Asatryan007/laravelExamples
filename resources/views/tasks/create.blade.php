a379c4bb032c792f619b581a6848eb41289a1c04
Asatryan007 <007asatryan@gmail.com>
2024-10-31 13:02:05 +0400

    Added responsive design to Edit blade

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,26 +4,27 @@
 
         <form action="{{ route('tasks.store') }}" method="POST" class="max-w-lg mx-auto p-6 bg-gray-500 rounded-lg">
             @csrf
+
             <div class="mb-4">
                 <label for="title" class="block text-black">Title</label>
                 <input type="text" name="title" required maxlength="255" class="w-full p-2 border border-white bg-gray-300 text-black">
                 @error('title')
-                <div class="alert alert-danger text-red-500">{{ $message }}</div>
+                    <div class="alert alert-danger text-red-500">{{ $message }}</div>
                 @enderror
             </div>
             <div class="mb-4">
                 <label for="description" class="block text-black">Description</label>
                 <textarea name="description" cols="30" rows="10" class="w-full p-2 border border-white bg-gray-300 text-black"></textarea>
                 @error('description')
-                <div class="alert alert-danger text-red-500">{{ $message }}</div>
+                    <div class="alert alert-danger text-red-500">{{ $message }}</div>
                 @enderror
             </div>
             <div class="mb-4">
-                <label for="startedAt" class="block text-black">Select a started date</label>
+                <label for="startedAt" class="block text-black">Choose the starting date.</label>
                 <input type="date" name="startedAt" class="w-full p-2 border border-white bg-gray-300 text-black focus:outline-none focus:border-blue-500">
             </div>
             <div class="mb-4">
-                <label for="deadline" class="block text-black">Deadline</label>
+                <label for="deadline" class="block text-black">Select the date for the deadline.</label>
                 <input type="date" name="deadline" class="w-full p-2 border border-white bg-gray-300 text-black">
             </div>
             <button type="submit" class="w-half  py-2 px-4 bg-blue-600 text-black rounded-lg shadow-md hover:bg-blue-700 transition duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">Create</button>
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,53 +1,55 @@
 <x-app-layout>
+    <div class="container mx-auto px-5">
+        <h2 class="text-white flex justify-center items-center text-2xl mb-4 mt-4">{{'Edit Task'}}</h2>
 
-    <h2>Edit Task</h2>
+        <form action="{{ route('tasks.update', $task) }}" method="POST" class="max-w-lg mx-auto p-6 bg-gray-500 rounded-lg">
+            @csrf
+            @method('PUT')
 
-    <form action="{{route('tasks.update', $task)}}" method="POST">
-        @csrf
-        @method('PUT')
-        <div>
-            <label for="title">Title</label>
-            <input type="text" name="title" value="{{$task->title}}" required>
-        </div>
-        <div>
-            <label for="description">Description</label>
-            <textarea name="description" id="" cols="30" rows="10">{{$task->description}}</textarea>
-        </div>
-        <div>
-            <label for="startedAt">Started At</label>
-            <input type="date" name="startedAt" value="{{$task->startedAt}}">
-        </div>
-        <div>
-            <label for="completedAt">Completed At</label>
-            <input type="date" name='completedAt' value="{{$task->completedAt}}">
-        </div>
-        <div>
-            <label for="deadline">Deadline</label>
-            <input type="date" name="deadline" value="{{$task->deadline}}">
-        </div>
-        <div>
-            <label for="status">Status</label>
-            <select name="status" id="status">
-                @foreach (App\Models\Task::statusOptionKeys() as $status)
-                    <option
-                        value="{{$status}}" {{ $task->status == $status ? 'selected' : '' }}>
-                        {{ App\Models\Task::statusLabel($status) }}
-                    </option>
-                @endforeach
-            </select>
-        </div>
-        <button type="submit">Update</button>
-        <a href="{{route('tasks.index')}}">
-            <button>Cancel</button>
-        </a>
-    </form>
-    @if ($errors->any())
-        <div>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+            <div class="mb-4">
+                <label for="title" class="block text-black">Title</label>
+                <input type="text" value="{{$task->title}}" name="title" required maxlength="255" class="w-full p-2 border border-white bg-gray-300 text-black">
+                @error('title')
+                <div class="alert alert-danger text-red-500">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="mb-8">
+                <label for="description" class="block text-black">Description</label>
+                <textarea name="description" cols="30" rows="10" class="w-full p-2 border border-white bg-gray-300 text-black">{{$task->description}}</textarea>
+                @error('description')
+                <div class="alert alert-danger text-red-500">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="mb-4">
+                <label for="startedAt" class="block text-black">Choose the starting date.</label>
+                <input type="date" name="startedAt" value="{{$task->startedAt}}" class="w-full p-2 border border-white bg-gray-300 text-black focus:outline-none focus:border-blue-500">
+            </div>
+            <div class="mb-4">
+                <label for="startedAt" class="block text-black">Choose the completion date.</label>
+                <input type="date" name="completedAt" value="{{$task->completedAt}}" class="w-full p-2 border border-white bg-gray-300 text-black focus:outline-none focus:border-blue-500">
+            </div>
+            <div class="mb-4">
+                <label for="deadline" class="block text-black">Select the date for the deadline.</label>
+                <input type="date" name="deadline" value="{{$task->deadline}}" class="w-full p-2 border border-white bg-gray-300 text-black">
+            </div>
+            <div class="mb-4">
+                <label for="status" class="block text-gray-700 mb-2">Status</label>
+                <select name="status" id="status" class="relative w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:border-blue-500">
+                    @foreach (App\Models\Task::statusOptionKeys() as $status)
+                        <option  value="{{ $status }}" {{ $task->status == $status ? 'selected' : '' }}>
+                            {{ App\Models\Task::statusLabel($status) }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="">
+                <button type="submit" class="bg-blue-500 text-white rounded-lg px-4 py-2 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                    {{'Update'}}
+                </button>
+                <button type="button" class="bg-gray-300 text-gray-700 rounded-lg px-4 py-2 hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-opacity-50">
+                    <a href="{{ route('tasks.index') }}"> {{'Cancel'}} </a>
+                </button>
+            </div>
+        </form>
+    </div>
 </x-app-layout>
