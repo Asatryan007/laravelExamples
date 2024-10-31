e8eafcf4ac901c859309e3d449a0de48a916573e
Asatryan007 <007asatryan@gmail.com>
2024-10-31 11:49:16 +0400

    Added responsive design to index.tasks blade:

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
-    <div class="text-white p-4">
+    <div class="container mx-auto text-white p-4">
         <h1 class="text-2xl font-bold mb-4">Tasks</h1>
 
         <a href="{{ route('tasks.create') }}" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mb-4 inline-block">Create Task</a>
 
-        <div class="filter mb-4">
+        <div class="filter mb-40">
             <h2 class="filter_title text-xl mb-2">Filter</h2>
             <form action="{{ route('tasks.index') }}" method="get" class="flex flex-col sm:flex-row items-start sm:items-center">
-                <label for="status" class="mr-2">Status</label>
+                <label for="status" class="mr-2">Status:</label>
                 <select class="text-black mb-2 sm:mb-0 sm:mr-2" name="status" id="status">
                     <option value="all" selected>All</option>
                     @php
@@ -19,7 +19,7 @@
                         </option>
                     @endforeach
                 </select>
-                <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">Get Values</button>
+                <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">{{'Apply Filter'}}</button>
             </form>
         </div>
 
@@ -30,7 +30,7 @@
         @if($tasks->isEmpty())
             <div>No active Task</div>
         @else
-            <div class="overflow-x-auto">
+            <div class="overflow-x-auto hidden md:flex">
                 <table class="min-w-full border-collapse border border-white">
                     <thead>
                     <tr>
@@ -48,13 +48,13 @@
                             <td class="border border-white p-2">{{ $task->startedAt ? $task->startedAt : 'non-privileged data' }}</td>
                             <td class="border border-white p-2">{{ $task->deadline ? $task->deadline : 'non-privileged data' }}</td>
                             <td class="border border-white p-2">{{ App\Models\Task::statusLabel($task->status) }}</td>
-                            <td class="border border-white p-2">
-                                <a href="{{ route('tasks.edit', $task) }}" class="text-blue-300 hover:underline">Edit</a>
-                                <a href="{{ route('tasks.show', $task) }}" class="text-blue-300 hover:underline">Show</a>
+                            <td class="flex justify-between border border-white p-2">
+                                <x-primary-button type="submit" class="w-20 justify-center hover:underline"><a  href="{{ route('tasks.edit', $task) }}">{{'Edit'}}</a></x-primary-button>
+                                <x-primary-button type="submit" class="w-20 hover:underline"><a href="{{route('tasks.show',$task)}}">{{'Show'}}</a></x-primary-button>
                                 <form action="{{ route('tasks.destroy', $task) }}" method="POST" class="inline">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit" class="text-red-400 hover:underline">Delete</button>
+                                    <x-primary-button type="submit" class="w-20 text-red-400 hover:underline">Delete</x-primary-button>
                                 </form>
                             </td>
                         </tr>
@@ -62,7 +62,40 @@
                     </tbody>
                 </table>
             </div>
-
+            <div class="flex overflow-x-auto justify-self-auto md:hidden ">
+                @foreach($tasks as $task)
+                    <table class="min-w-fit mr-5 border-collapse border border-white">
+                        <tr>
+                            <th class="border border-white p-2">Title</th>
+                            <td class="border border-white p-2">{{$task->title}}</td>
+                        </tr>
+                        <tr>
+                            <th class="border border-white p-2">Started At</th>
+                            <td class="border border-white p-2">{{ $task->startedAt ? $task->startedAt : 'non-privileged data' }}</td>
+                        </tr>
+                        <tr>
+                            <th class="border border-white p-2">Deadline</th>
+                            <td class="border border-white p-2">{{ $task->deadline ? $task->deadline : 'non-privileged data' }}</td>
+                        </tr>
+                        <tr>
+                            <th class="border border-white p-2">Status</th>
+                            <td class="border border-white p-2">{{ App\Models\Task::statusLabel($task->status) }}</td>
+                        </tr>
+                        <tr>
+                            <th class="border border-white p-2">Actions</th>
+                            <td class="border border-white p-2">
+                                <x-primary-button type="submit" class="w-20 justify-center hover:underline"><a  href="{{ route('tasks.edit', $task) }}">{{'Edit'}}</a></x-primary-button>
+                                <x-primary-button type="submit" class="w-20 hover:underline"><a href="{{route('tasks.show',$task)}}">{{'Show'}}</a></x-primary-button>
+                                <form action="{{ route('tasks.destroy', $task) }}" method="POST" class="inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <x-primary-button type="submit" class="w-20 text-red-400 hover:underline">Delete</x-primary-button>
+                                </form>
+                            </td>
+                        </tr>
+                    </table>
+                @endforeach
+            </div>
             {{ $tasks->appends(['status' => request('status', 'all')])->links() }}
         @endif
     </div>
